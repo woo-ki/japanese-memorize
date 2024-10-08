2c84bbf9194d43baf145521a5a5112fdbfaded38
김영욱 <ziziza93@naver.com>
2024-10-09 06:11:57 +0900

    pwa추가

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,20 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from '@styles/theme.emotion.ts';
 import { RouterProvider } from 'react-router-dom';
 
+// 앱의 엔트리 포인트(main.tsx 등)에서 서비스 워커 등록
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
   const envDir = `${process.cwd()}`;
@@ -16,6 +17,46 @@ export default defineConfig(({ mode }) => {
         jsxImportSource: '@emotion/react',
       }),
       tsconfigPaths(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: '일본어 암기장',
+          short_name: '일본어 암기장',
+          start_url: '/',
+          description: 'JLPT N1 ~ N5까지의 단어를 외워 보세요',
+          display: 'standalone',
+          theme_color: '#f7f3e9',
+          icons: [
+            {
+              src: '/pwa/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/pwa/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+        workbox: {
+          // 전체 오프라인 캐싱 비활성화
+          runtimeCaching: [
+            {
+              // 특정 경로의 파일만 캐싱
+              urlPattern: /\/assets\/fonts\/Hangeuljaemin4-Regular\/.*\.(woff|woff2|ttf|otf)$/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'font-cache',
+                expiration: {
+                  maxEntries: 10, // 최대 10개 파일만 캐싱
+                  maxAgeSeconds: 60 * 60 * 24 * 30, // 30일 동안 캐시 유지
+                },
+              },
+            },
+          ],
+        },
+      }),
     ],
     server: {
       port: Number(env.VITE_APP_PORT),
